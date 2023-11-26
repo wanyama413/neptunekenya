28ec808612a2dfd4c27521192d3fbd7fbdb6f8a3
Sammy Mathias Wanyama <99073324+wanyama413@users.noreply.github.com>
2023-11-26 19:41:11 +0300

    update

--- src/pages/NewsArticle.jsx
+++ src/pages/NewsArticle.jsx
@@ -14,7 +14,6 @@ const NewsArticle = () => {
         setNews(data);
       } catch (error) {
         console.error("Error fetching news:", error);
-        // Handle the error as needed
       }
     };
 
